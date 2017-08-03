3745cc6821228d897e7a5a46da15dc8b223cba2b
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2017-08-03 18:37:27 +1200

    WK4TorialC

---
+++ C#/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create var
+            var Fruits = new string[5];
+            //Loop start
+            var count = 5;
+            for (var i = 0; i < count; i++)
+            {
+                //Fruits = i + 1;
+            //User input
+            Console.WriteLine("Pick a Fruit");
+            Fruits = Console.ReadLine();
+            }
+           
+
+            //Print In Ascending Order
+            Array.Sort(Fruits);
+            Console.WriteLine(string.Join(",",Fruits));
+
+            //Print in Descending Order.
+            Array.Reverse(Fruits);
+            Console.WriteLine(String.Join(",", Fruits));
+
+            //Close
+            Console.ReadKey();
+        }
+    }
+}
